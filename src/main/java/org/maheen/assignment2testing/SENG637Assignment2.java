37425dda664a5cfe03fe78d0dcf926986dd8c507
Maheen Hoque <mashrurmaheen@gmail.com>
2026-02-25 00:14:05 -0700

    Add initial project structure with .gitignore, pom.xml, and main Java class

---
+++ src/main/java/org/maheen/assignment2testing/SENG637Assignment2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.maheen.assignment2testing;
+
+/**
+ *
+ * @author Maheen
+ */
+public class SENG637Assignment2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
